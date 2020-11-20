c9b456cbc419c0a1bf15a78c23c3e6d33ba41ffd
ASUS <ASUS@ASUS-X541U>
2020-11-21 06:26:20 +0700

    Program ini bertujuan menampilkan nama dan umur barang antik

---
+++ src/pboif2/pkg10119044/latihan56/umurbarangantik/PBOIF210119044Latihan56UmurBarangAntik.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan nama dan umur barang antik
+ */
+
+public class PBOIF210119044Latihan56UmurBarangAntik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Radio radio = new Radio(234);
+        radio.setName("Radio AM");
+        System.out.println("Nama barang Antik : "+radio.getName());
+        radio.tampilUmur();
+    }
+}
---
+++ src/pboif2/pkg10119044/latihan56/umurbarangantik/Radio.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan nama dan umur barang antik
+ */
+public class Radio extends BarangAntik{
+    private String name;
+    
+    public Radio(int umur) {
+        super(umur);
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+}
